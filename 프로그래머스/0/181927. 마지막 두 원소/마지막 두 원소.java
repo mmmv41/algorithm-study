ed9ecd2fa5166b10a7157167fc628e5fa6e13f7c
minkyung <156222089+mmmv41@users.noreply.github.com>
2025-01-08 20:59:44 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,19 +1,12 @@
-import java.util.ArrayList;
-
 class Solution {
-    public ArrayList<Integer> solution(int[] num_list) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        
-        for (int i = 0; i<num_list.length; i++) 
-            answer.add(num_list[i]);
-        
-        if (num_list[num_list.length -1 ] > num_list[num_list.length - 2]) {
-            answer.add(num_list[num_list.length -1 ] - num_list[num_list.length -2]); 
-        } 
-        else {
-            answer.add(num_list[num_list.length -1] * 2); 
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length+1];
+
+        for(int i=0; i<num_list.length; i++){
+            answer[i] = num_list[i];
         }
-            
-        return answer ;
+
+        answer[num_list.length] = num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list[num_list.length - 1] - num_list[num_list.length - 2] : num_list[num_list.length - 1] * 2;
+        return answer;
     }
 }
