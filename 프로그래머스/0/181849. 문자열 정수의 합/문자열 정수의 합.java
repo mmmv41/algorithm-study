385e01fdb132cbc1199589ca9f0b0bbb171496b7
minkyung <156222089+mmmv41@users.noreply.github.com>
2025-02-16 23:48:40 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.04 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String num_str) {
         int count = 0;
         for (char c : num_str.toCharArray()) {
-            count += c - '0'; // '0'의 아스키 코드(48)를 빼면 숫자가 됨
+            count += c - '0'; // '0'의 아스키 코드(48)를 빼면 숫자 됨
         }
         return count;
     }
