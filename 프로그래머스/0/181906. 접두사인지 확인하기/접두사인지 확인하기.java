1ba324be543f37e30c94e572500bf89cf663e6ad
minkyung <156222089+mmmv41@users.noreply.github.com>
2025-02-17 14:10:58 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        if (my_string.length() < is_prefix.length()){
+            return 0;
+        }
+        
+        int length = is_prefix.length();
+        
+        if (my_string.substring(0,length).equals(is_prefix)){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
