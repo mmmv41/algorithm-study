567ddcf3cd737620077886bf01851cebf4a653bf
minkyung <156222089+mmmv41@users.noreply.github.com>
2025-02-25 23:10:07 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -3,9 +3,12 @@ class Solution {
         int size = 0;
         if (names.length % 5 != 0 && names.length > 5){
             size = names.length / 5 + 1;
-        } else {
+        } else if (names.length % 5 == 0) {
             size = names.length / 5;
+        } else {
+            size = 1;
         }
+        
         String[] result = new String[size];
         for (int i = 0, cnt = 0; i < names.length; i+=5, cnt++){
             result[cnt] = names[i];
