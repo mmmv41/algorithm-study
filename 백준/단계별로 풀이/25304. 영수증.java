d0255a7d3bba4bd9a5c8720ac6bc919d5fa4aac2
mmmv41 <tlsalsrud5068@daum.net>
2025-03-20 20:55:25 +0900

    P25304 영수증

---
+++ 백준/단계별로 풀이/25304. 영수증.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int total = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            total += a * b;
+        }
+        br.close();
+
+        if (total == X) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
