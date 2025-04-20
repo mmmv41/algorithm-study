27a6e23cf617d9433c1c5ea601ed7849c47bd2fe
mmmv41 <tlsalsrud5068@daum.net>
2025-04-20 16:56:43 +0900

    P10809 알파벳 찾기-1

---
+++ 백준/단계별로 풀이/10809 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,18 @@
+// charAt() 이용한 코드 (120ms)
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String word = br.readLine();
+        for (char c = 'a'; c <= 'z'; c++) {
+            sb.append(word.indexOf(c) + " ");   // 찾는 알파벳이 없다면 -1 반환
+        }
+        System.out.print(sb);
+    }
+}
