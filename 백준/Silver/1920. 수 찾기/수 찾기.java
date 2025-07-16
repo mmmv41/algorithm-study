3a1c72864efea8e342ae2ec712e57b2e4e6b5773
minkyung <156222089+mmmv41@users.noreply.github.com>
2025-07-16 19:21:29 +0900

    [Silver IV] Title: 수 찾기, Time: 564 ms, Memory: 43856 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,39 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        arr = new int[n];
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int a = Integer.parseInt(st.nextToken());
-            arr[i] = a;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int cnt = 0;
+        Arrays.sort(arr);
         int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            int b = Integer.parseInt(st.nextToken());
 
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == b) {
-                    cnt = 1;
-                    break;
-                } else {
-                    cnt = 0;
-                }
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (binarySearch(a) >= 0) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
             }
-            sb.append(cnt).append("\n");
         }
         System.out.print(sb);
+    }
+
+    static int binarySearch(int num) {
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (num < arr[mid]) {
+                hi = mid - 1;
+            } else if (num > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
