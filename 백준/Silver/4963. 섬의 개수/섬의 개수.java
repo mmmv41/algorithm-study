b0368f5704dd0edf5b5ba5c33c04759d4740bb5e
minkyung <156222089+mmmv41@users.noreply.github.com>
2025-08-06 22:47:58 +0900

    [Silver II] Title: 섬의 개수, Time: 152 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,50 +1,49 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int w, h;
-    static int[][] map;
     static boolean[][] visited;
-
+    static int[][] arr;
     static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
     static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         while (true) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
 
-            if (w == 0 && h == 0) break;
+            if (w == 0 && h == 0) {
+                break;
+            }
 
-            map = new int[h][w];
+            arr = new int[h][w];
             visited = new boolean[h][w];
 
             for (int i = 0; i < h; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < w; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            int count = 0;
+            int cnt = 0;
 
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
+                    if (arr[i][j] == 1 && !visited[i][j]) {
                         dfs(i, j);
-                        count++;
+                        cnt++;
                     }
                 }
             }
-
-            System.out.println(count);
+            System.out.println(cnt);
         }
     }
 
@@ -56,7 +55,7 @@ public class Main {
             int ny = y + dy[i];
 
             if (nx >= 0 && ny >= 0 && nx < h && ny < w) {
-                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                if (arr[nx][ny] == 1 && !visited[nx][ny]) {
                     dfs(nx, ny);
                 }
             }
